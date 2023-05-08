b1f508469ec78cd9ec4ff69437554ac7903e732e
AKRAM Md Waseem <MAM11@dsone.3ds.com>
2023-05-08 21:08:17 +0530

    firebase setup done

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
-import 'firebase/compat/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyANgnskjq471VsjSwBuoL9dcw36S23GqpI",
-    authDomain: "cart-9a9b3.firebaseapp.com",
-    projectId: "cart-9a9b3",
-    storageBucket: "cart-9a9b3.appspot.com",
-    messagingSenderId: "249307652721",
-    appId: "1:249307652721:web:2986dd4110e547519af1d0"
-  };
-  
-  // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyANgnskjq471VsjSwBuoL9dcw36S23GqpI",
+  authDomain: "cart-9a9b3.firebaseapp.com",
+  projectId: "cart-9a9b3",
+  storageBucket: "cart-9a9b3.appspot.com",
+  messagingSenderId: "249307652721",
+  appId: "1:249307652721:web:2986dd4110e547519af1d0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
   const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
